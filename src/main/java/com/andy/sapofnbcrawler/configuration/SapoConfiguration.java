908d64192029e252b853114f8a0571fd848902c8
nhk.nguyen <NHK.Nguyen@VN-DEV_ANDY.tsb.co.kr>
2024-12-31 16:13:39 +0700

    - Andy 20241231: Adding common response for all api

---
+++ src/main/java/com/andy/sapofnbcrawler/configuration/SapoConfiguration.java
@@ -0,0 +1,8 @@
+package com.andy.sapofnbcrawler.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SapoConfiguration {
+
+}
---
+++ src/main/java/com/andy/sapofnbcrawler/response/CommonResponse.java
@@ -0,0 +1,21 @@
+package com.andy.sapofnbcrawler.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+import lombok.Data;
+
+@Data
+@JsonRootName("respone")
+@JsonInclude(Include.NON_NULL)
+public class CommonResponse {
+	
+	@JsonProperty("status")
+	private String status;
+	@JsonProperty("message")
+	private String message;
+	@JsonProperty("data")
+	private Object data;
+}
